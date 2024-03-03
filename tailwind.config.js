321af10a9b57923d007619a8d05b4c1418108538
Arley Souto <macx15@hotmail.com>
2024-03-02 21:56:43 -0300

    chore: configurado tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["./src/**/*.{jsx,js,ts, tsx}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+export default {
+  content: [
+    './src/**/*.{ts,tsx}',
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
