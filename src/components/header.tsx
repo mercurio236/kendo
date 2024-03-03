aad22d2ee969423b325f290d5cd8d9928c34635e
Arley Souto <macx15@hotmail.com>
2024-03-03 04:08:31 -0300

    feat: finalizado

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,11 @@
-
-
-export function Header(){
-    return(
-        <div className="border-b">
-            <div className="flex h-16 items-center gap-6 px-6">
-                Header
-            </div>
-        </div>
-    )
+const IMG_GIF =
+  'https://developers.giphy.com/branch/master/static/header-logo-0fec0225d189bc0eae27dac3e3770582.gif'
+export function Header() {
+  return (
+    <div className="border-b">
+      <div className="flex h-16 items-center gap-6 px-6">
+        <img src={IMG_GIF} className="w-[300px]" alt="" />
+      </div>
+    </div>
+  )
 }
